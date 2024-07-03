cd0575b73b25c639cb1709a6ac0c09e0a8a4dd88
Aslan <nurlybekov.aslan@yandex.kz>
2024-07-03 05:13:39 +0300

    Task2 Terraform

--- lab5/learn-terraform-docker-container/main.tf
+++ lab5/learn-terraform-docker-container/main.tf
@@ -16,10 +16,20 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.image_id
-  name  = "tutorial"
+  name  = var.container_name
 
   ports {
     internal = 80
-    external = 8000
+    external = 8080
   }
+}
+
+output "container_id" {
+  description = "ID of the Docker container"
+  value       = docker_container.nginx.id
+}
+
+output "image_id" {
+  description = "ID of the Docker image"
+  value       = docker_image.nginx.id
 }
